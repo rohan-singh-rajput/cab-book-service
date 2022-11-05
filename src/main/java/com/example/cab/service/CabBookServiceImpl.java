fc90021478653bf689d9b7c2eb4789f1cfcae27e
rohan-singh-rajput <rohansinghrajput135@gmail.com>
2022-11-05 20:11:04 +0530

    minor refactoring

--- src/main/java/com/example/cab/service/CabBookServiceImpl.java
+++ src/main/java/com/example/cab/service/CabBookServiceImpl.java
@@ -15,53 +15,54 @@ import java.util.List;
 
 @Service
 public class CabBookServiceImpl implements CabBookService {
-	
+
 	private List<CabDriver> availableCabs = new ArrayList<>();
-	
-    @Autowired
-    private CabDriverRepository cabDriverRepository;
-    @Autowired
-    private UserRepository userRepository;
 
-    @Override
-    public List<RideDetails> getAllRideDetails() {
-        return null;
-    }
+	@Autowired
+	private CabDriverRepository cabDriverRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Override
-    public List<CabDriver> findRides(RideDetails rideDetails) throws UserNotFoundException {
-        if(!ifUserAvailable(rideDetails.getName())){
-            throw new UserNotFoundException("User Not Found");
-        }
-        
-        for(CabDriver cabs : cabDriverRepository.getDriverDetails()){
-        	if(distanceBetweenEndPoints(rideDetails.getStart(),cabs.getLocation()) <= 5 && cabs.isAvailable()){
-        		availableCabs.add(cabs);
-        		}
-            }
-        return availableCabs;
-    }
+	@Override
+	public List<RideDetails> getAllRideDetails() {
+		return null;
+	}
 
-    public Double distanceBetweenEndPoints(Coordinate user,Coordinate cab) {
-        return Math.sqrt((cab.getY() - user.getY())*(cab.getY() - user.getY()) + ((cab.getX() - user.getX())*(cab.getX() - user.getX())));
-    }
+	@Override
+	public List<CabDriver> findRides(RideDetails rideDetails) throws UserNotFoundException {
+		if (!ifUserAvailable(rideDetails.getName())) {
+			throw new UserNotFoundException("User Not Found");
+		}
 
-    public boolean ifUserAvailable(String name){
-        for (User user : userRepository.getAllUsers()) {
-            if (user.getName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
+		for (CabDriver cabs : cabDriverRepository.getDriverDetails()) {
+			if (distanceBetweenEndPoints(rideDetails.getStart(), cabs.getLocation()) <= 5 && cabs.isAvailable()) {
+				availableCabs.add(cabs);
+			}
+		}
+		return availableCabs;
+	}
+
+	public Double distanceBetweenEndPoints(Coordinate user, Coordinate cab) {
+		return Math.sqrt((cab.getY() - user.getY()) * (cab.getY() - user.getY())
+				+ ((cab.getX() - user.getX()) * (cab.getX() - user.getX())));
+	}
+
+	public boolean ifUserAvailable(String name) {
+		for (User user : userRepository.getAllUsers()) {
+			if (user.getName().equals(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public String chooseRide(ChooseRide ride) {
-		 if(ifUserAvailable(ride.getUserName())){
-			for(CabDriver cab: availableCabs) {
-				if(cab.getName().equals(ride.getCabDriverName())) {
+		if (ifUserAvailable(ride.getUserName())) {
+			for (CabDriver cab : availableCabs) {
+				if (cab.getName().equals(ride.getCabDriverName())) {
 					cab.setAvailable(false);
-					return "Booked by "+ride.getUserName() + " "+ride.getCabDriverName();
+					return "Booked by " + ride.getUserName() + " " + ride.getCabDriverName();
 				}
 			}
 		}
